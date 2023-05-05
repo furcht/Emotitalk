cbe1a5b63506f1985d41c3adc5918cd4ee165afe
Ruben Villanueva <furcht@rubens-air.lan>
2023-05-05 10:28:18 -0700

    Secure Emotitalk

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
 
   const initializeSocket = async () => {
     // let sox = await fetch('ws://0.0.0.0:3005/');
-    socket = io('ws://emotitalk.net/');
+    socket = io('https://emotitalk.net/');
 
     socket.on('connect', () => {
       socket.emit("connected");
